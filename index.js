e29525919a1f043ba6ef240c317554a4003fa753
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2019-06-17 11:34:35 -0400

    Connect to a drive shared with the service account
    * Initialise a drive and authorise
    * Log out files in the drive to test connection

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const { google } = require('googleapis');
+const credentials = require('./credentials.json');
+
+const scopes = [
+    'https://www.googleapis.com/auth/drive'
+];
+
+// create a JSON Web Token object to pass as an argument when calling the Drive API
+const auth = new google.auth.JWT(
+    credentials.client_email, 
+    null, 
+    credentials.private_key, 
+    scopes
+);
+
+const drive = google.drive({ version: 'v3', auth });
+
+// passing an empty object as a default argument
+drive.files.list({}, (err, res) => {
+    if (err) throw err;
+    const files = res.data.files;
+    if (files.length) {
+        files.map((file) => {
+            console.log(file);
+        });
+    } else {
+        console.log(`No files found`);
+    }
+});
